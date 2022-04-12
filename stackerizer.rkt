0bd8584bb511d3ebcdba63a13af653447bd931a3
jpaquim <joaopaquim@gmail.com>
2022-04-12 16:00:31 +0100

    Add first iteration of stackerizer

---
+++ stackerizer.rkt
@@ -0,0 +1,11 @@
+#lang br/quicklang
+(provide + *)
+
+(define-macro (stackerizer-mb EXPR)
+  #'(#%module-begin
+     EXPR))
+(provide (rename-out [stackerizer-mb #%module-begin]))
+
+(define-macro-cases +
+  [(+ FIRST) #'FIRST]
+  [(+ FIRST NEXT ...) #'(list '+ FIRST (+ NEXT ...))])
