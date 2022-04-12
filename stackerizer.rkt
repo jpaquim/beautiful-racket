296fbbf6e30d520dcd6543472febd6274c39d840
jpaquim <joaopaquim@gmail.com>
2022-04-12 16:19:52 +0100

    Use ellipsis in outer define-ops macro

--- stackerizer.rkt
+++ stackerizer.rkt
@@ -6,11 +6,12 @@
      (for-each displayln (reverse (flatten EXPR)))))
 (provide (rename-out [stackerizer-mb #%module-begin]))
 
-(define-macro (define-op OP)
-  #'(define-macro-cases OP
-      [(OP FIRST) #'FIRST]
-      [(OP FIRST NEXT (... ...))
-       #'(list 'OP FIRST (OP NEXT (... ...)))]))
+(define-macro (define-ops OP ...)
+  #'(begin
+      (define-macro-cases OP
+        [(OP FIRST) #'FIRST]
+        [(OP FIRST NEXT (... ...))
+         #'(list 'OP FIRST (OP NEXT (... ...)))])
+      ...))
 
-(define-op +)
-(define-op *)
+(define-ops + *)
