0a94558e9eca00045d12f0b855e3db17e4b07b1d
jpaquim <joaopaquim@gmail.com>
2022-04-14 22:25:41 +0100

    Use module+ to extender reader module

--- wires/main.rkt
+++ wires/main.rkt
@@ -1,10 +1,12 @@
 #lang br/quicklang
-(module reader br/quicklang
-  (provide read-syntax)
-  (define (read-syntax path port)
-    (define wire-datums
-      (for/list ([wire-str (in-lines port)])
-        (format-datum '(wire ~a) wire-str)))
-    (strip-bindings
-     #`(module wires-mod wires/main
-         #,@wire-datums))))
+
+(module+ reader
+  (provide read-syntax))
+
+(define (read-syntax path port)
+  (define wire-datums
+    (for/list ([wire-str (in-lines port)])
+      (format-datum '(wire ~a) wire-str)))
+  (strip-bindings
+   #`(module wires-mod wires/main
+       #,@wire-datums)))
