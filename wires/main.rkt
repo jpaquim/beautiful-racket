e3c40b4cfbc91ee50717904fff5c38c1f0d4430c
jpaquim <joaopaquim@gmail.com>
2022-04-14 22:50:40 +0100

    Add wires expander macros

--- wires/main.rkt
+++ wires/main.rkt
@@ -10,3 +10,37 @@
   (strip-bindings
    #`(module wires-mod wires/main
        #,@wire-datums)))
+
+(provide #%module-begin)
+
+(define-macro-cases wire
+  [(wire ARG -> ID)
+   #'(define/display (ID) (val ARG))]
+  [(wire OP ARG -> ID)
+   #'(wire (OP (val ARG)) -> ID)]
+  [(wire ARG1 OP ARG2 -> ID)
+   #'(wire (OP (val ARG1) (val ARG2)) -> ID)]
+  [else #'(void)])
+(provide wire)
+
+(define-macro (define/display (ID) BODY)
+  #'(begin
+      (define (ID) BODY)
+      (module+ main
+        (displayln (format "~a: ~a" 'ID (ID))))))
+
+(define (val num-or-wire)
+  (if (number? num-or-wire)
+      num-or-wire
+      (num-or-wire)))
+
+(define (mod-16bit x) (modulo x 65536))
+(define-macro (define-16bit ID PROC-ID)
+  #'(define ID (compose1 mod-16bit PROC-ID)))
+
+(define-16bit AND bitwise-and)
+(define-16bit OR bitwise-ior)
+(define-16bit NOT bitwise-not)
+(define-16bit LSHIFT arithmetic-shift)
+(define (RSHIFT x y) (LSHIFT x (- y)))
+(provide AND OR NOT LSHIFT RSHIFT)
