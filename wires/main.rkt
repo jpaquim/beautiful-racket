7b6c259c6b3c6d53bbdca394924133f54e76a2fc
jpaquim <joaopaquim@gmail.com>
2022-04-14 22:24:16 +0100

    Add wires reader

---
+++ wires/main.rkt
@@ -0,0 +1,10 @@
+#lang br/quicklang
+(module reader br/quicklang
+  (provide read-syntax)
+  (define (read-syntax path port)
+    (define wire-datums
+      (for/list ([wire-str (in-lines port)])
+        (format-datum '(wire ~a) wire-str)))
+    (strip-bindings
+     #`(module wires-mod wires/main
+         #,@wire-datums))))
