e5dc54b1ee036ddd4d06371bd411ff355b10c306
jpaquim <joaopaquim@gmail.com>
2022-04-21 17:41:29 +0100

    Functions instead of macros as alternative

--- basic/expr.rkt
+++ basic/expr.rkt
@@ -22,3 +22,24 @@
 (define-macro-cases b-expt
   [(_ VAL) #'VAL]
   [(_ LEFT "^" RIGHT) #'(expt LEFT RIGHT)])
+
+#;(define-cases b-sum
+    [(_ arg) arg]
+    [(_ left op right) ((case op
+                          [("+") +]
+                          [("-") -]) left right)])
+
+#;(define-cases b-product
+    [(_ arg) arg]
+    [(_ left op right) ((case op
+                          [("*") *]
+                          [("/") (λ (l r) (/ l r 1.0))]
+                          [("mod") modulo]) left right)])
+
+#;(define-cases b-neg
+    [(_ val) val]
+    [(_ _ val) (- val)])
+
+#;(define-cases b-expt
+    [(_ val) val]
+    [(_ left _ right) (expt left right)])
