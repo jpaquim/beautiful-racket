23b79d1ecf8669d39a8731496c70b25fb00e56d9
jpaquim <joaopaquim@gmail.com>
2022-04-21 17:10:54 +0100

    Add variable and input support to basic

--- basic/expander.rkt
+++ basic/expander.rkt
@@ -6,10 +6,21 @@
 (define-macro (b-module-begin (b-program LINE ...))
   (with-pattern
       ([((b-line NUM STATEMENT ...) ...) #'(LINE ...)]
-       [(LINE-FUNC ...) (prefix-id "line-" #'(NUM ...))])
+       [(LINE-FUNC ...) (prefix-id "line-" #'(NUM ...))]
+       [(VAR-ID ...) (find-unique-var-ids #'(LINE ...))])
     #'(#%module-begin
+       (define VAR-ID 0) ...
        LINE ...
        (define line-table
          (apply hasheqv (append (list NUM LINE-FUNC) ...)))
        (void (run line-table)))))
 (provide (rename-out [b-module-begin #%module-begin]))
+
+(begin-for-syntax
+  (require racket/list)
+  (define (find-unique-var-ids line-stxs)
+    (remove-duplicates
+     (for/list ([stx (in-list (stx-flatten line-stxs))]
+                #:when (syntax-property stx 'b-id))
+       stx)
+     #:key syntax->datum)))
