c2189b094812ed1639f158faf3e28516085bce29
jpaquim <joaopaquim@gmail.com>
2022-04-21 17:38:42 +0100

    Add other expression types to basic

---
+++ basic/sample-3.rkt
@@ -0,0 +1,11 @@
+#lang basic
+10 print -1 + 2 - 3 * (4 + 5) / 6 ^ 7 + 8 mod 9
+20 print 1 + 2 * 3 rem 7
+30 print (1 + 2) * 3 rem 9
+40 print 3 + - 4 rem -1
+50 print - 2 ^ 4 rem -16
+60 print (- 2) ^ 4 rem 16
+70 print 24 / 4 / 2 rem 3
+80 print 24 / (4 / 2) rem 12
+90 print 2 ^ 3 ^ 2 rem 64
+100 print 2 ^ (3 ^ 2) rem 512
