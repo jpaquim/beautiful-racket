219c8ebd7002faaf0f53d729e3b3f92acef01e25
jpaquim <joaopaquim@gmail.com>
2022-04-22 20:19:17 +0100

    Add command line arg support to basic

--- basic/expander.rkt
+++ basic/expander.rkt
@@ -11,27 +11,48 @@
        [(IMPORT-NAME ...)
         (find-property 'b-import-name #'(LINE ...))]
        [(EXPORT-NAME ...)
-        (find-property 'b-export-name #'(LINE ...))])
+        (find-property 'b-export-name #'(LINE ...))]
+       [((SHELL-ID SHELL-IDX) ...)
+        (make-shell-ids-and-idxs caller-stx)]
+       [(UNIQUE-ID ...)
+        (unique-ids
+         (syntax->list #'(VAR-ID ... SHELL-ID ...)))])
     #'(#%module-begin
        (module configure-runtime br
          (require basic/setup)
          (do-setup!))
        (require IMPORT-NAME ...)
        (provide EXPORT-NAME ...)
-       (define VAR-ID 0) ...
+       (define UNIQUE-ID 0) ...
+       (let ([clargs (current-command-line-arguments)])
+         (set! SHELL-ID (get-clarg clargs SHELL-IDX)) ...)
        LINE ...
        (define line-table
          (apply hasheqv (append (list NUM LINE-FUNC) ...)))
        (parameterize
            ([current-output-port (basic-output-port)])
-       (void (run line-table))))))
+         (void (run line-table))))))
 (provide (rename-out [b-module-begin #%module-begin]))
+
+(define (get-clarg clargs idx)
+  (if (<= (vector-length clargs) idx)
+      0
+      (let ([val (vector-ref clargs idx)])
+        (or (string->number val) val))))
 
 (begin-for-syntax
   (require racket/list)
+
+  (define (unique-ids stxs)
+    (remove-duplicates stxs #:key syntax->datum))
+
   (define (find-property which line-stxs)
-    (remove-duplicates
+    (unique-ids
      (for/list ([stx (in-list (stx-flatten line-stxs))]
                 #:when (syntax-property stx which))
-       stx)
-     #:key syntax->datum)))
+       stx)))
+
+  (define (make-shell-ids-and-idxs ctxt)
+    (define arg-count 10)
+    (for/list ([idx (in-range arg-count)])
+      (list (suffix-id #'arg idx #:context ctxt) idx))))
